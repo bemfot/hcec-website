f0c450e314429824cf5a93dc957b42fe0c935355
Tobi Oladele <oladeletobiadedoyin@gmail.com>
2025-12-14 12:39:37 +0100

    base url updated

--- app/api/daily-honey/route.ts
+++ app/api/daily-honey/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
   const body = await req.text();
 
-  const res = await fetch(`${process.env.BASE_URL}/daily-honey`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/daily-honey`, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
     body: body || undefined,
